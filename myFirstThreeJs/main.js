574e96f37e81de198f339c6ff5f18036206fc386
SudaraWishvajith <it21221378@my.sliit.lk>
2023-07-14 21:18:30 +0530

    add stars
    add multiple images by a funciton called addStar, make stars from SphereGeometry and meshStandardMaterial and use MathUtils.randFloatSpread to randomly position them

--- myFirstThreeJs/main.js
+++ myFirstThreeJs/main.js
@@ -34,6 +34,19 @@ const gridHelper = new THREE.GridHelper(200, 50);
 
 const controls = new OrbitControls (camera, renderer.domElement);
 
+function addStar(){
+  const geometry = new THREE.SphereGeometry(0.25);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff});
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
 
 scene.add(lightHelper, gridHelper);
 
