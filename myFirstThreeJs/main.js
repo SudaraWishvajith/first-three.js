d588a4801f6455455f861ff9cfa1db96f8e33d88
SudaraWishvajith <it21221378@my.sliit.lk>
2023-07-14 22:49:34 +0530

    Create Earth
    replace the previous ring shape object with earth

--- myFirstThreeJs/main.js
+++ myFirstThreeJs/main.js
@@ -16,19 +16,26 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 //renderer.render(scene, camera);
+const earthTexture = new THREE.TextureLoader().load('earth.jpg');
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-const torus = new THREE.Mesh(geometry, material);
+const earth = new THREE.Mesh(
+  new THREE.SphereGeometry( 5, 32, 16 ),
+  new THREE.MeshStandardMaterial({
+    map: earthTexture
+  })
 
-scene.add(torus);
+);
+
+scene.add(earth);
 
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(10,5,15);
+pointLight.position.set(10,5,15,10000);
 
 const ambiantLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambiantLight);
 
+const spaceBackground = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceBackground;
 const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper = new THREE.GridHelper(200, 50);
 
@@ -46,17 +53,14 @@ function addStar(){
 }
 
 Array(200).fill().forEach(addStar);
-
-
 scene.add(lightHelper, gridHelper);
-
 
 function animate() {
   requestAnimationFrame( animate );
 
-  torus.rotation.x += 0.01; 
-  torus.rotation.y += 0.05;
-  torus.rotation.z += 0.01;
+  //earth.rotation.x += 0.001; 
+  earth.rotation.y += 0.01;
+  //earth.rotation.z += 0.01;
 
   controls.update();
   
